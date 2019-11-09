d86c9e6eabdf1108ff7bd396271b2ab9d23ddc22
Musyoka Muasya <josephmuasya8@gmail.com>
2019-11-09 12:22:05 +0300

    More work on checkout item

---
+++ app/src/main/java/com/andromeda/immicart/delivery/DeliveryDetailsFragment.kt
@@ -0,0 +1,24 @@
+package com.andromeda.immicart.delivery
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.andromeda.immicart.R
+
+
+class DeliveryDetailsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_delivery_details, container, false)
+    }
+
+
+}
