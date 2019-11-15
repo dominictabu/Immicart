d2efa2521f161130dd489502d236e6884a953e94
Musyoka Muasya <josephmuasya8@gmail.com>
2019-11-15 05:07:57 +0300

    Days setUp

--- app/src/main/java/com/andromeda/immicart/delivery/delivery_schedule/DaysAdapter.kt
+++ app/src/main/java/com/andromeda/immicart/delivery/delivery_schedule/DaysAdapter.kt
@@ -1,16 +1,22 @@
 package com.andromeda.immicart.delivery.delivery_schedule
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.Adapter
+import com.andromeda.immicart.R
 import java.util.*
 
-class DaysAdapter : Adapter<DaysAdapter.DaysViewHolder>() {
+class DaysAdapter(var context: Context) : Adapter<DaysAdapter.DaysViewHolder>() {
 
     var deliveryDays = listOf<DeliveryDay>()
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DaysViewHolder {
 
+
+        val layout = LayoutInflater.from(context).inflate(R.layout.delivery_schedule, parent, false)
+        return DaysViewHolder(layout)
 
     }
 
