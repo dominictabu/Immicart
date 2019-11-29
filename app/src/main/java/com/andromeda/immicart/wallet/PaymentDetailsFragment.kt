2e16eb8614af43d3523a4c59a05c0f434d8c1d69
Musyoka Muasya <josephmuasya8@gmail.com>
2019-11-29 18:50:53 +0300

    Worked with java version of recharge wallet

--- app/src/main/java/com/andromeda/immicart/wallet/PaymentDetailsFragment.kt
+++ app/src/main/java/com/andromeda/immicart/wallet/PaymentDetailsFragment.kt
@@ -1,41 +1,36 @@
 package com.andromeda.immicart.wallet
 
-import android.content.Context
-import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.fragment.app.Fragment
+import com.androidstudy.daraja.Daraja
+import com.androidstudy.daraja.DarajaListener
+import com.androidstudy.daraja.model.AccessToken
 import com.andromeda.immicart.R
+import com.andromeda.immicart.checkout.CheckoutActivity
+import com.andromeda.immicart.checkout.Constants
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Activities that contain this fragment must implement the
- * [PaymentDetailsFragment.OnFragmentInteractionListener] interface
- * to handle interaction events.
- * Use the [PaymentDetailsFragment.newInstance] factory method to
- * create an instance of this fragment.
- *
- */
 class PaymentDetailsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-    private var listener: OnFragmentInteractionListener? = null
+    lateinit var daraja: Daraja
+    var phoneNumber: String = ""
+    var paybillNumber = "174379" // The default
+    val passKey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919"
+    var amountPayable = "" //Fetch this information from the amount
+
+    var partyA = ""
+    var partyB = ""
+    val callbackUrl = "" //Use our callback url
+
+    var accountReference = ""
+    var transactionDescription = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
@@ -44,60 +39,24 @@ class PaymentDetailsFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_payment_details, container, false)
-    }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    fun onButtonPressed(uri: Uri) {
-        listener?.onFragmentInteraction(uri)
-    }
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        if (context is OnFragmentInteractionListener) {
-            listener = context
-        } else {
-            throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
-        }
-    }
-
-    override fun onDetach() {
-        super.onDetach()
-        listener = null
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     *
-     *
-     * See the Android Training lesson [Communicating with Other Fragments]
-     * (http://developer.android.com/training/basics/fragments/communicating.html)
-     * for more information.
-     */
-    interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        fun onFragmentInteraction(uri: Uri)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PaymentDetailsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PaymentDetailsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+        daraja = Daraja.with(
+            Constants.IMMICART_CONSUMER_KEY,
+            Constants.IMMICART_CONSUMER_SECRET,
+            object : DarajaListener<AccessToken> {
+                override fun onResult(accessToken: AccessToken) {
                 }
-            }
+
+                override fun onError(error: String) { //             TODO  Show error message
+                }
+            })
+
+
     }
+
+
+
+
+
 }
+
